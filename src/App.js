cc4e2f1abd011d2b8f0fb411ea05612aa2602f92
akshaybhavnacorp <akshay.kumar@bhavnacorp.com>
2022-10-28 12:47:47 +0530

    Basic_Amenities

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Basic from './BasicAmenities/Basic';
+
+function App() {
+  return (
+    <div>
+      <Basic> </Basic>
+    </div>
+  );
+}
+
+export default App
