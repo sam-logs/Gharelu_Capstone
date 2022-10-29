199a05ec5a6c9dad81f891d13a2c9539db61e646
Saumya Prabhakar <Saumya.Prabhakar@bhavnacorp.com>
2022-10-29 15:19:41 +0530

    Frontend

--- src/components/Profileinfo.jsx
+++ src/components/Profileinfo.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import Base from './Base'
 import { getUser } from './services/user.service'
 import userContext from './userContext'
+
 function ProfileInfo() {
   const object = useContext(userContext)
   const {userId} = useParams()
